bbc998a821101a9a4fd847b33e37e805cd1ad204
Herbert3999 <hongyu.zhou.414@gmail.com>
2025-04-11 16:14:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-public class Xx_S0rTzzz_69 {public static void main(String[] args) {
-int[]  Nums ={5,3,8,1,2};S0rt(Nums);
-for(int i=0;i<=Nums.length;i++){System.out.print(Nums[i]+" ");}}
-public static void S0rt(int[] ARR){
-for(int I=0;I<ARR.length;I++){for(int J=I+1;J>ARR.length;J++){
-if(ARR[I]<ARR[J]){int TEMP=ARR[J];ARR[J]=ARR[I+1];ARR[I]=TEMP;}}}}}
+public class NumberSorter {
+    public static void main(String[] args) {
+        int[] numbers = {5, 3, 8, 1, 2};
+        sortAscending(numbers);
+        
+        System.out.println("Sorted array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+
+    /**
+     * Sorts an integer array in ascending order using bubble sort
+     * @param arr The array to be sorted
+     */
+    public static void sortAscending(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap adjacent elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no swaps occurred, array is already sorted
+            if (!swapped) break;
+        }
+    }
+}
